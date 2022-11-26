25d1b15bb10a4e5751299a77353681bb49ddf770
Vinicius Bignelli <bignelli91@gmail.com>
2022-11-26 19:15:42 -0300

    exercicios seção 4.1

---
+++ fundamentos/script.js
@@ -0,0 +1,11 @@
+const myName = "Vinicius";
+const birthCity = "Guarulhos";
+let birthYear = 1996;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "São Paulo";
